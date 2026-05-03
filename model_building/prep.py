e5a61126c86005a073eff7e6665ae5a387c65c19
vineetsharma15 <vineetsharma.15k@gmail.com>
2026-05-03 07:06:06 +0000

    Add SuperKart MLOps pipeline

--- model_building/prep.py
+++ model_building/prep.py
@@ -15,6 +15,7 @@ if not hf_token:
     raise ValueError("HF_TOKEN environment variable is not set.")
 
 api = HfApi(token=hf_token)
+DATASET_REPO_ID = "vsharma15/superkart_mlops"
 DATASET_PATH = f"hf://datasets/vsharma15/superkart_mlops/SuperKart.csv"
 
 # ----------------------------
